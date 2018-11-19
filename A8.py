1e91396d67faac44c2d87af3cf9b7c3e466b3c07
amaalrehmatullah <45183364+amaalrehmatullah@users.noreply.github.com>
2018-11-19 17:13:27 -0500

    Update A8.py

--- A8.py
+++ A8.py
@@ -1,35 +1,75 @@
-class Customer():
+class Customer(object):
+
   def __init__(self, cust_name, email, phone, address):
+
     self.cust_name = cust_name
+
     self.email = email
+
     self.phone = phone
+
     self.address = address
+
     membershipAcct = []
+
     
-  def nameInput(self):
+
+  def nameInput():
+
     NI = input("Please enter your name: ")
-    return NI
+
+    return (NI)
+
   
-  def emailInput(self):
-    EI = input(int("Please enter your phone num: "))
-    return EI
+  def phoneInput():
+    PI = input(str("Please enter your phone num: "))
+
+    return(PI)
+
   
-  def addressInput(self):
-    AI = input("Please enter your address: "))
-    rerun AI
+
+  def addressInput():
+
+    AI = input("Please enter your address: ")
+
+    return(AI)
+
   
-  def validateMembership(self):
+
+  def validateMembership(self, NI):
+    membershipAcct = []
     VM = input("Do you have a membership with us: (y/n)?")
+
     if VM == 'n':
-      membershipAcct.append(VM)
+
+      membershipAcct.append(NI)
+      print(membershipAcct)
+
       return "Congratualations, your membership is now created, and you are an official member"
+
     
+
     else:
+
       return "You already have a membership with us!"
+
    
-  def __str__(self):class
-    return(str(self.cust_name) + ',' + str(self.email) + ',' + str(self.phone) + ',' + str(self.address))
-  
+
+  #def __str__():
+
+#    return(str(NI + ',' + PI + ',' + AI + ',' + VM))
+
+
+#global code---------------------------------------------------------------------
+print(Customer.nameInput())
+print(Customer.phoneInput())
+print(Customer.addressInput())
+print(Customer.validateMembership())
+#print(__str__())
+#print(membershipAcct)
+
+
+
  
  Shelf():
   def __init__(self, title, genre, author, ISBN, cust_name):
@@ -50,3 +90,14 @@ class Customer():
       self.count = count
     def __str__(self):
       return(str(self.title) +','+ str(self.genre) +','+ str(self.author) +','+ str(self.ISBN) +','+ str(self.count))
+
+  
+  
+  
+#global code---------------------------------------------------------------------
+print(Customer.nameInput())
+print(Customer.phoneInput())
+print(Customer.addressInput())
+print(Customer.validateMembership())
+#print(__str__())
+#print(membershipAcct)
