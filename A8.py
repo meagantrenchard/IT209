353ffd2cc0c76acbc02975b48d98e6a37da67bd3
meagantrenchard <42045946+meagantrenchard@users.noreply.github.com>
2018-11-19 17:00:09 -0500

    Update A8.py

--- A8.py
+++ A8.py
@@ -6,26 +6,47 @@ class Customer():
     self.address = address
     membershipAcct = []
     
-  def nameInput():
+  def nameInput(self):
     NI = input("Please enter your name: ")
-    print(NI)
+    return NI
   
-  def emailInput():
+  def emailInput(self):
     EI = input(int("Please enter your phone num: "))
-    print(EI)
+    return EI
   
-  def addressInput():
+  def addressInput(self):
     AI = input("Please enter your address: "))
-    print(AI)
+    rerun AI
   
-  def validateMembership():
+  def validateMembership(self):
     VM = input("Do you have a membership with us: (y/n)?")
     if VM == 'n':
       membershipAcct.append(VM)
-      print("Congratualations, your membership is now created, and you are an official member")
-      
+      return "Congratualations, your membership is now created, and you are an official member"
     
-  def __str__(self):
+    else:
+      return "You already have a membership with us!"
+   
+  def __str__(self):class
     return(str(self.cust_name) + ',' + str(self.email) + ',' + str(self.phone) + ',' + str(self.address))
-class Book():
-class Shelf():
+  
+ 
+ Shelf():
+  def __init__(self, title, genre, author, ISBN, cust_name):
+    self.title = title
+    self.genre = genre
+    self.author = author
+    self.ISBN = ISBN
+    self.cust_name = cust_name
+  def __str__(self):
+    return(str(self.title) +','+ str(self.genre) +','+ str(self.author) +','+ str(self.ISBN) +','+ str(self.cust_name))
+
+  class Book():
+    def __init__(self, title, genre, author, ISBN, count):
+      self.title = title
+      self.genre = genre
+      self.author = author
+      self.ISBN = ISBN
+      self.count = count
+    def __str__(self):
+      return(str(self.title) +','+ str(self.genre) +','+ str(self.author) +','+ str(self.ISBN) +','+ str(self.count))
