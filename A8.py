7507280d3d9f7d31a1769482b158cf0e0797e3f1
meagantrenchard <42045946+meagantrenchard@users.noreply.github.com>
2018-11-19 16:36:15 -0500

    Update A8.py

--- A8.py
+++ A8.py
@@ -1,4 +1,10 @@
 class Customer():
-  def __init__(self, cust_name, email, phone, address)
+  def __init__(self, cust_name, email, phone, address):
+    self.cust_name = cust_name
+    self.email = email
+    self.phone = phone
+    self.address = address
+  def __str__(self):
+    return(str(self.cust_name) + ',' + str(self.email) + ',' + str(self.phone) + ',' + str(self.address))
 class Book():
 class Shelf():
